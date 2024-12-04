50f04f191861e188943c5c5b6d890668c83122be
Romain Veya <veya.romain@gmail.com>
2024-12-04 09:54:46 +0100

    merged bitbucket repo in github repo

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, redirect, url_for, jsonify, request
+import nfc
+import jwt
+import time
+import base64
+SECRET_KEY = '069332689e9ea429cbe681fbe05b3c403490a38208a0d15cbe967069d18490b7'
+ALGORITHM = 'HS256'
+
+app= Flask(__name__)
+
+def read_nfc():
+    # Initialize the NFC reader
+    clf = nfc.ContactlessFrontend('usb')
+
+    if not clf:
+        raise RuntimeError("NFC reader not found. Please connect the device.")
+
+    print("Waiting for NFC badge...")
+
+    # Variable to store the UID
+    card_uid = None
+
+    def on_connect(tag):
+        nonlocal card_uid  # Access the variable defined outside this function
+        print("Badge detected!")
+        card_uid = tag.identifier.hex().upper()  # Convert UID to a readable hex string
+        print(f"Card UID: {card_uid}")
+        return False  # Disconnect immediately after reading the tag
+
+    # Wait for a tag to be presented
+    while True:
+        try:
+            clf.connect(rdwr={'on-connect': on_connect})
+            if card_uid:
+                clf.close()
+                print(f"Returning UID: {card_uid}")
+                return card_uid
+        except Exception as e:
+            print(f"Error while reading NFC badge: {e}")
+            clf.close()
+            break
+def get_signing_key():
+    # Decode the Base64-encoded secret key
+    key_bytes = base64.b64decode(SECRET_KEY)
+    return key_bytes
+        
+@app.route('/')
+def index():
+    return render_template('Simba.html')
+
+@app.route('/waiting', methods=['GET'])
+def wait_for_nfc():
+    cardUID = read_nfc()
+    if cardUID:
+        # Create the JWT token with the NFC data
+        token = jwt.encode({"card_uid": cardUID}, get_signing_key(), algorithm=ALGORITHM)
+        print(cardUID)
+        print(token)
+        
+        # Return the redirection URL as a JSON response
+        redirect_url = f"http://localhost:8080/simba/external/api/v1/pad-dashboard/login?token={token}"
+        return jsonify({"redirect_url": redirect_url})
+    
+    return jsonify({"error": "No NFC data received"}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ static/js/script.js
@@ -0,0 +1,88 @@
+const RANDOM_IMAGE_URL = "http://localhost:8080/simba/external/api/v1/pad-dashboard/image";
+
+document.addEventListener("load", onInit());
+
+function onInit(){
+    document.getElementById("randomImage").src = RANDOM_IMAGE_URL;
+}
+
+const badgeCard = document.querySelector('.badge-card');
+const arrow = document.querySelector('.arrow-right');
+const noBadgeToggledContent = document.querySelector('.no-badge-content');
+const noAccountToggledContent = document.querySelector('.no-account-content');
+const noBadgeToggledButton = document.querySelector('.toggle-button-top');
+const noAccountToggledButton = document.querySelector('.toggle-button-bottom');
+
+var noBadgeToggled = false;
+var noAccountToggled = false;
+
+badgeCard.addEventListener('click', launchAnimation);
+fetchData();
+
+async function fetchData() {
+  try {
+      const response = await fetch('/waiting');
+      const data = await response.json();
+      if (data.redirect_url) {
+        await launchAnimation();
+        window.location.href = data.redirect_url;
+      } else {
+          console.error('Error: No redirect URL in response');
+      }
+  } catch (error) {
+      console.error('Error calling the API:', error);
+  }
+}
+
+function launchAnimation() {
+    return new Promise((resolve) => {
+        console.log("test 3");
+        badgeCard.style.animation = 'none';
+        void badgeCard.offsetWidth;
+        badgeCard.style.animation = 'badged 1.5s forwards';
+
+        arrow.style.display = 'none';
+
+        badgeCard.addEventListener('animationend', function() {
+            badgeCard.style.position = 'fixed';
+            badgeCard.style.right = 'calc(50% - 165px)';
+            badgeCard.style.bottom = 'calc(50% - 100px)';
+            badgeCard.style.transform = 'scale(40) rotate(0deg)';
+            resolve();
+        }, { once: true });
+    });
+}
+
+const BORDER = '2px solid rgb(102, 102, 102)';
+
+function toggleAccordion(number){
+    if(number === 1){
+        if(noBadgeToggled){
+            noBadgeToggledContent.style.display = 'none';
+            noBadgeToggledButton.style.borderBottom = BORDER;
+            noBadgeToggled = false;
+        } else {
+            noBadgeToggledContent.style.display = 'block';
+            noBadgeToggledButton.style.borderBottom = 'none';
+            noAccountToggledButton.style.borderBottom = BORDER;
+            noBadgeToggled = true;
+        }
+
+        noAccountToggledContent.style.display = 'none';
+        noAccountToggled = false;
+    } else if (number === 2){
+        if(noAccountToggled){
+            noAccountToggledContent.style.display = 'none';
+            noAccountToggledButton.style.borderBottom = BORDER;
+            noAccountToggled = false;
+        } else {
+            noAccountToggledContent.style.display = 'block';
+            noAccountToggledButton.style.borderBottom = 'none';
+            noBadgeToggledButton.style.borderBottom = BORDER;
+            noAccountToggled = true;
+        }
+
+        noBadgeToggledContent.style.display = 'none';
+        noBadgeToggled = false;
+    }
+}
